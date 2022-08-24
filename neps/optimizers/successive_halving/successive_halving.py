ae13db3288f2b9592e68963c8520dab7832e9bd1
neeratyoy <neeratyoy@gmail.com>
2022-08-24 14:25:23 +0200

    Adding number of configs mapping to rungs in SH

--- neps/optimizers/successive_halving/successive_halving.py
+++ neps/optimizers/successive_halving/successive_halving.py
@@ -1,24 +1,21 @@
 from __future__ import annotations
 
-import random
-from typing import Any
 from copy import deepcopy
 
 import numpy as np
 import pandas as pd
+from metahyper.api import ConfigResult
 from typing_extensions import Literal
 
 from ...search_spaces.numerical.integer import IntegerParameter
-from metahyper.api import ConfigResult, instance_from_map
-
 from ...search_spaces.search_space import SearchSpace
 from ..base_optimizer import BaseOptimizer
-from .sampling_policy import SamplingPolicy, RandomUniformPolicy
 from .promotion_policy import PromotionPolicy
+from .sampling_policy import RandomUniformPolicy, SamplingPolicy
 
 
 class SuccessiveHalving(BaseOptimizer):
-    """Implements a SuccessiveHalving procedure with arbitrary sampling and promotion policies."""
+    """Implements a SuccessiveHalving procedure with a sampling and promotion policy."""
 
     def __init__(
         self,
@@ -27,46 +24,43 @@ class SuccessiveHalving(BaseOptimizer):
         eta: int = 4,
         early_stopping_rate: int = 0,
         initial_design_type: Literal["max_budget", "unique_configs"] = "max_budget",
-        sampling_policy: SamplingPolicy = None,
+        sampling_policy: SamplingPolicy = RandomUniformPolicy,
         promotion_policy: PromotionPolicy = None,
-        loss_value_on_error: None | float = None,
-        cost_value_on_error: None | float = None,
-        
     ):
-        super().__init__(
-            pipeline_space=pipeline_space,
-            budget=budget
-        )
+        super().__init__(pipeline_space=pipeline_space, budget=budget)
         self.min_budget = pipeline_space.fidelity.lower
         self.max_budget = pipeline_space.fidelity.upper
         self.eta = eta
+        # SH implicitly sets early_stopping_rate to 0
+        # the parameter is exposed to allow HB to call SH with different stopping rates
         self.early_stopping_rate = early_stopping_rate
-        self.sampling_policy = sampling_policy 
-        self.promotion_policy = promotion_policy 
-        
+        self.sampling_policy = sampling_policy
+        self.promotion_policy = promotion_policy
+
         # `max_budget_init` checks for the number of configurations that have been
         # evaluated at the target budget
         self.initial_design_type = initial_design_type
 
         # check to ensure no rung ID is negative
+        # equivalent to s_max in https://arxiv.org/pdf/1603.06560.pdf
         self.stopping_rate_limit = np.floor(
             np.log(self.max_budget / self.min_budget) / np.log(self.eta)
         ).astype(int)
         assert self.early_stopping_rate <= self.stopping_rate_limit
 
-        # maps rungs to a fidelity value
+        # maps rungs to a fidelity value for an SH bracket with `early_stopping_rate`
         self.rung_map = self._get_rung_map(self.early_stopping_rate)
-        print(self.rung_map)
+        self.config_map = self._get_config_map(self.early_stopping_rate)
+
         self.max_rung = len(self.rung_map) - 1
         self.fidelities = list(self.rung_map.values())
         # stores the observations made and the corresponding fidelity explored
         # crucial data structure used for determining promotion candidates
         self.observed_configs = pd.DataFrame([], columns=("config", "rung", "perf"))
         # stores which configs occupy each rung at any time
-        self.rung_members: dict = {k: [] for k in self.rung_map.keys()}
-        # one-to-one with self.rung_members, storing corresponding performance
-        self.rung_members_performance: dict = {k: [] for k in self.rung_map.keys()}
-        self.rung_promotions: dict = dict()
+        self.rung_members: dict = {}  # stores config IDs per rung
+        self.rung_members_performance: dict = dict()  # performances recorded per rung
+        self.rung_promotions: dict = dict()  # records a promotable config per rung
         self.total_fevals = 0
 
     def _get_rung_map(self, s: int = 0) -> dict:
@@ -89,6 +83,26 @@ class SuccessiveHalving(BaseOptimizer):
             )
             _max_budget /= self.eta
         return rung_map
+
+    def _get_config_map(self, s: int = 0) -> dict:
+        """Maps rungs (0,1,...,k) to the number of configs for each fidelity"""
+        assert s <= self.stopping_rate_limit
+        new_min_budget = self.min_budget * (self.eta**s)
+        nrungs = (
+            np.floor(np.log(self.max_budget / new_min_budget) / np.log(self.eta)).astype(
+                int
+            )
+            + 1
+        )
+        s_max = self.stopping_rate_limit + 1
+        _s = self.stopping_rate_limit - s
+        # L2 from Alg 1 in https://arxiv.org/pdf/1603.06560.pdf
+        _n_config = np.floor(s_max / (_s + 1)) * self.eta**_s
+        config_map = dict()
+        for i in reversed(range(nrungs)):
+            config_map[i] = int(_n_config)
+            _n_config //= self.eta
+        return config_map
 
     # TODO: check pending
     def _load_previous_observations(
@@ -126,7 +140,7 @@ class SuccessiveHalving(BaseOptimizer):
         Args:
             previous_results (dict[str, ConfigResult]): [description]
             pending_evaluations (dict[str, ConfigResult]): [description]
-        """    
+        """
         # TODO: Read in rungs using the config id (alternatively, use get/load state)
         self.observed_configs = pd.DataFrame([], columns=("config", "rung", "perf"))
 
@@ -197,12 +211,15 @@ class SuccessiveHalving(BaseOptimizer):
             if top_k > 0:
                 if self.promotion_policy is None:
                     self.rung_promotions[_rung] = np.array(self.rung_members[_rung])[
-                        np.argsort(self.rung_members_performance[_rung])[
-                            :top_k]].tolist()
+                        np.argsort(self.rung_members_performance[_rung])[:top_k]
+                    ].tolist()
                 else:
                     promotion_kwargs = {}
                     self.rung_promotions[_rung] = self.promotion_policy.promote(
-                        k, self.rung_members[_rung], self.rung_members_performance[_rung], **promotion_kwargs).tolist()
+                        self.rung_members[_rung],
+                        self.rung_members_performance[_rung],
+                        **promotion_kwargs,
+                    ).tolist()
 
         return
 
@@ -232,7 +249,6 @@ class SuccessiveHalving(BaseOptimizer):
             val = len(_observed_configs) <= self._initial_design_size
         return val
 
-
     def get_config_and_ids(  # pylint: disable=no-self-use
         self,
     ) -> tuple[SearchSpace, str, str | None]:
@@ -240,7 +256,7 @@ class SuccessiveHalving(BaseOptimizer):
 
         Returns:
             [type]: [description]
-        """    
+        """
         rung_to_promote = self.is_promotable()
         if rung_to_promote is not None:
             # promote existing config
@@ -260,23 +276,20 @@ class SuccessiveHalving(BaseOptimizer):
                 )
                 fidelity_value = self.rung_map[0]  # base rung is always 0
                 config.fidelity.value = fidelity_value
-                print('Sampled the config from pipeline space.')
+                print("Sampled the config from pipeline space.")
             else:
                 # TODO acquire arguments for the sampling policy
                 sampling_args = {}
                 config = self.sampling_policy.sample(**sampling_args)
                 fidelity_value = self.rung_map[0]  # base rung is always 0
                 config.fidelity.value = fidelity_value
-                
+
             previous_config_id = None
             config_id = f"{len(self.observed_configs)}_0"
 
-        print('Config:', config.hp_values())
+        print("Config:", config.hp_values())
         return config.hp_values(), config_id, previous_config_id  # type: ignore
 
 
-
-
-
-if __name__ == '__main__':
-    sh = SuccesiveHalving()
+if __name__ == "__main__":
+    pass
